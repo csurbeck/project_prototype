9a0dd00cb4174554ad6508ce750b6255faaee0b0
csurbeck <118086236+csurbeck@users.noreply.github.com>
2023-03-29 16:16:48 -0700

    Add files via upload
    prototype

---
+++ sketch.js
@@ -0,0 +1,77 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(220);
+  if (mouseX > 50 && mouseX < 100 && mouseY > 50 && mouseY < 300) {
+    fill(0);
+    rect(50, 55, 50, 240, 10);
+  } else {
+    fill(255);
+    rect(50, 50, 50, 250, 10);
+  }
+  if (mouseX > 100 && mouseX < 150 && mouseY > 50 && mouseY < 300) {
+    fill(0);
+    rect(100, 55, 50, 240, 10);
+  } else {
+    fill(255);
+    rect(100, 50, 50, 250, 10);
+  }
+  if (mouseX > 150 && mouseX < 200 && mouseY > 50 && mouseY < 300) {
+    fill(0);
+    rect(150, 55, 50, 240, 10);
+  } else {
+    fill(255);
+    rect(150, 50, 50, 250, 10);
+  }
+  if (mouseX > 200 && mouseX < 250 && mouseY > 50 && mouseY < 300) {
+    fill(0);
+    rect(200, 55, 50, 240, 10);
+  } else {
+    fill(255);
+    rect(200, 50, 50, 250, 10);
+  }
+  if (mouseX > 250 && mouseX < 300 && mouseY > 50 && mouseY < 300) {
+    fill(0);
+    rect(250, 55, 50, 240, 10);
+  } else {
+    fill(255);
+    rect(250, 50, 50, 250, 10);
+  }
+  if (mouseX > 300 && mouseX < 350 && mouseY > 50 && mouseY < 300) {
+    fill(0);
+    rect(300, 55, 50, 240, 10);
+  } else {
+    fill(255);
+    rect(300, 50, 50, 250, 10);
+  }
+  if (mouseX > 350 && mouseX < 400 && mouseY > 50 && mouseY < 300) {
+    fill(0);
+    rect(350, 55, 50, 240, 10);
+  } else {
+    fill(255);
+    rect(350, 50, 50, 250, 10);
+  }
+  if (mouseX > 400 && mouseX < 450 && mouseY > 50 && mouseY < 300) {
+    fill(0);
+    rect(400, 55, 50, 240, 10);
+  } else {
+    fill(255);
+    rect(400, 50, 50, 250, 10);
+  }
+  if (mouseX > 450 && mouseX < 500 && mouseY > 50 && mouseY < 300) {
+    fill(0);
+    rect(450, 55, 50, 240, 10);
+  } else {
+    fill(255);
+    rect(450, 50, 50, 250, 10);
+  }
+  if (mouseX > 500 && mouseX < 550 && mouseY > 50 && mouseY < 300) {
+    fill(0);
+    rect(500, 55, 50, 240, 10);
+  } else {
+    fill(255);
+    rect(500, 50, 50, 250, 10);
+  }
+}
